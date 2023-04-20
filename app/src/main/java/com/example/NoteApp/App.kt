9df7255408fb863bb2d0acfd92e19ed59fb7b71b
dan4uone <legeng300@mail.ru>
2023-04-20 17:36:42 +0600

    initial commit

---
+++ app/src/main/java/com/example/NoteApp/App.kt
@@ -0,0 +1,9 @@
+package com.example.NoteApp
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class App: Application()
+// Создал ветку main и перехожу на dev чтобы стянуть изменения через git pull
+// Стянул сверху написанное через ветку dev и обратно перешёл на main чтобы merge сделатьпш
